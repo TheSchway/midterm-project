9857a829851bbef207a28c35b4cb52d0a987f62d
Joshua Dennis <jdenn11@u.rochester.edu>
2019-03-17 11:48:08 -0400

    Started the project

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # midterm-project
+
+
+
+## Information Architecture
+
+### Ontology
+
+- A game has a general overview
+- A game has gameplay
+- A game has a plot
+
+### Taxonomy
+
+- General overview is referred to as "overview"
+- Gameplay is referred to as "gameplay"
+- Plot is referred to as "plot"
+
+### Choreography
+
+On the more information page, I chose to present the "overview" section first because it was the most broad of the three topics and would serve as a good entry point for viewers. Next, I included the "gameplay" section due to the relative importance of the information, given the nature of the topic (a video game). Finally, I included the "plot" section as it is the most in-depth/complicated section and is the final pull to convince readers to sign up for the newsletter (which is now the primary goal since the viewer has already progressed to the second page).
+
+
+
+## Design
+
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home | Midterm Project</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans|IBM+Plex+Serif" rel="stylesheet">
+</head>
+<body class="grid-layout">
+	<div class="head">
+		<header>
+			<a href="index.php">
+				<figure>
+					<h1>
+						<img src="images/overwatch-logo.png" alt="Overwatch Logo">
+					</h1>
+				</figure>
+			</a>
+		</header>
+	</div>
+
+	<div class="nav-bar">
+		<nav class="menu">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="info.php">More Info</a></li>
+			</ul>
+		</nav>
+	</div>
+
+	<div class="weak">
+		<aside class="quote">
+			<p>Heroes Never Die!</p>
+			<p>-Mercy</p>
+		</aside>
+	</div>
+
+	<div class="main">
+		<ol class="boxes">
+			<li><a href="info.php">Learn More!</a></li>
+		</ol>
+	</div>
+	
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+</body>
+</html>
