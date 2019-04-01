785a110f947fa9ad3a176af33af01278bc42488f
Joshua Dennis <jdenn11@u.rochester.edu>
2019-04-01 15:27:38 -0400

    Things

--- info.php
+++ info.php
@@ -19,27 +19,27 @@
 
 	<div class="overview">
 		<article class="flex-container">
-			<div>
+			<div class="down">
 				<h2>Overview</h2>
+			</div>
+			<div class="information">
 				<p>Overwatch is a team-based multiplayer first-person shooter developed and published by Blizzard Entertainment and released on May 24, 2016 for PlayStation 4, Xbox One, and Windows. Described as a "hero shooter", Overwatch assigns players into two teams of six, with each player selecting from a roster of 30 characters, known as "heroes", each with a unique style of play whose roles are divided into three general categories that fit their role. Players on a team work together to secure and defend control points on a map or escort a payload across the map in a limited amount of time. Players gain cosmetic rewards that do not affect gameplay, such as character skins and victory poses, as they play the game. The game was initially launched with casual play, with a competitive ranked mode, various 'arcade' game modes, and a player-customizable server browser subsequently included following its release. Additionally, Blizzard has added new characters, maps, and game modes post-release, all free of charge, with the only additional cost to players being optional loot boxes to earn cosmetic items.</p>
 			</div>
-			<div class="shift">
-				<p>Overwatch is Blizzard's fourth major franchise and came about following the 2014 cancellation of the ambitious massively multiplayer online role-playing game Titan. A portion of the Titan team came up with the concept of Overwatch, based on the success of team-based first-person shooters like Team Fortress 2 and the growing popularity of multiplayer online battle arenas, creating a hero-based shooter that emphasized teamwork. Some elements of Overwatch borrow assets and concepts from the canceled Titan project. After establishing the narrative of an optimistic near-future Earth setting after a global crisis, the developers aimed to create a diverse cast of heroes that spanned genders and ethnicities as part of this setting. Significant time is spent adjusting the balance of the characters, making sure that new players would still be able to have fun while skilled players would present each other with a challenge.</p>
-			</div>
-			<div class="shift">
-				<p>Overwatch was unveiled at BlizzCon 2014 in a fully playable state and was in a closed beta from late 2015 through early 2016. An open beta in May 2016 drew in nearly 10 million players. The release of the game was promoted with short animated videos to introduce the narrative and characters. Overwatch received universal acclaim from critics, who praised the game for its accessibility, diverse appeal of its hero characters, bright cartoonish art style, and enjoyable gameplay. Blizzard reported over US$1 billion in revenue during the first year of its release, and had more than 40 million players after two years. Overwatch is considered one of the greatest video games of all time, receiving numerous game of the year awards, alongside other accolades. The game has also become recognized as an esport, with Blizzard helping to fund and produce professional leagues, such as the Overwatch League.</p>
+			<div class="picture-two">
+				<figure>
+					<img src="images/blizzard.png" alt="Blizzard">
+				</figure>
 			</div>
 		</article>
 	</div>
 
 	<div class="gameplay">
 		<article class="flex-container">
-			<div>
+			<div class="down-two">
 				<h2>Gameplay</h2>
-				<p>Overwatch features a number of different game modes, principally designed around squad-based combat with two opposing teams of six players each. Players select one of over two dozen pre-made hero characters from one of three class types: Damage heroes that deal most of the damage to attack or defend control points, Tank heroes that can absorb a large amount of damage, and Support heroes that provide healing or other buffs for their teammates. Each hero has a unique skill kit, defining their intrinsic attributes like health points and running speed, their primary attacks, several active and passive skills, and an ultimate ability that can only be used after it has been charged through dealing damage. Players can change their hero during the course of a match, as a goal of Overwatch's design was to encourage dynamic team compositions that adapt to the situation. The game's genre has been described by some journalists as a "hero shooter", due to its design around specific heroes and classes.</p>
 			</div>
-			<div class="move">
-				<p>The game features game modes for casual play, competitive ranked play, and for supporting esports competitions including Blizzard's Overwatch League. These modes generally are centered around sequentially securing control of points on the map, or escorting a payload between points on the map, with one team attacking while the other defends. Other modes set aside for casual matches include solo and team deathmatch, capture-the-flag, and unique modes run during various seasonal events. Regardless of winning or losing a match, players gain experience towards a player level, and on gaining a new level, receive loot boxes that contain cosmetic items that they can use to customize the appearance of the hero characters but otherwise does not affect gameplay. Loot boxes can also be purchased through microtransactions.</p>
+			<div class="information-three">
+				<p>Overwatch features a number of different game modes, principally designed around squad-based combat with two opposing teams of six players each. Players select one of over two dozen pre-made hero characters from one of three class types: Damage heroes that deal most of the damage to attack or defend control points, Tank heroes that can absorb a large amount of damage, and Support heroes that provide healing or other buffs for their teammates. Each hero has a unique skill kit, defining their intrinsic attributes like health points and running speed, their primary attacks, several active and passive skills, and an ultimate ability that can only be used after it has been charged through dealing damage. Players can change their hero during the course of a match, as a goal of Overwatch's design was to encourage dynamic team compositions that adapt to the situation. The game's genre has been described by some journalists as a "hero shooter", due to its design around specific heroes and classes.</p>
 			</div>
 			<div>
 				<figure class="picture">
@@ -51,15 +51,17 @@
 
 	<div class="plot">
 		<article class="flex-container">
-			<div>
+			<div class="down-two">
 				<h2>Plot</h2>
+			</div>
+			<div class="information-two">
 				<p>The backstory to Overwatch is described through animated shorts and other information distributed by Blizzard in promoting the game. Overwatch is set sixty years into the future of a fictionalized Earth, thirty years after the resolution of what is known as the "Omnic Crisis". Prior to the Omnic Crisis, humanity had been in a golden age of prosperity and technology development. Humans developed robots with artificial intelligence called "Omnics", which were put to use to achieve economic equality, and began to be treated as people in their own right. The Omnic Crisis began when the worldwide automated "omnium" facilities that produced them, started producing a series of lethal, hostile robots, that attacked humankind. The United Nations quickly formed Overwatch, an international task force to combat this threat and restore order.</p>
+				<footer>(Source: <a href="https://en.wikipedia.org/wiki/Overwatch_(video_game)" target="blank"> Overwatch Wikipedia Page)</a></footer>
 			</div>
-			<div class="move">
-				<p>Two veteran soldiers were put in charge of Overwatch: Gabriel Reyes and Jack Morrison. Though Overwatch successfully quelled the robotic uprising and brought a number of talented individuals to the forefront, a rift ended up developing between Reyes and Morrison, and Morrison became the leader of Overwatch while Reyes took charge of Blackwatch, Overwatch's covert operations division. Overwatch maintained peace across the world for several decades in what was called the "Overwatch Generation" as the team gained more members, but the rift between Morrison and Reyes intensified. Several allegations of wrongdoing and failures were leveled at Overwatch, leading to a public outcry against the organization and in-fighting between its members, prompting the UN to investigate the situation. During this, an explosion destroyed Overwatch's headquarters in Switzerland, purportedly killing Morrison and Reyes among others. The UN passed the Petras Act, which dismantled Overwatch and forbade any Overwatch-type activity.</p>
-			</div>
-			<div class="move">
-				<p>Overwatch is set six years after the Petras Act; without Overwatch, corporations have started to take over, fighting and terrorism have broken out in parts of the globe, and there are signs of a second Omnic Crisis occurring in Russia. A former member of Overwatch, the intelligent gorilla Winston, decides to begin reforming Overwatch to protect the peace once again despite the Petras Act, with the team members recruiting old friends and gaining new allies in their fight. Thus it is also revealed that Reyes and Morrison were not killed in the explosion, that, in fact, was the result of a battle between them: Morrison became a masked vigilante known as "Soldier: 76", who is trying to uncover the reasons why Overwatch was shut down; while Reyes joined the criminal organisation Talon and became "Reaper", a terrorist disguised as Death.</p>
+			<div>
+				<figure class="picture-three">
+					<img src="images/heroes.jpg" alt="Heroes">
+				</figure>
 			</div>
 		</article>
 	</div>
@@ -73,6 +75,6 @@
 				<input type="submit" value="Submit">
 			</fieldset>
 		</form>
-		<footer>Source: <a href="https://en.wikipedia.org/wiki/Overwatch_(video_game)" target="blank"> Overwatch Wikipedia Page</a></footer>
 	</div>
+		
 <?php include "inc/scripts.inc"; ?>
